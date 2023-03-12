cd2911fd8d9b224e7f8140c21e5641558ed14654
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-03-12 18:36:19 +0530

    client and server setup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-red-600">Hello world!!!</div>;
+  return (
+    <div>
+      <h2 className="m-4">Hello world!!!</h2>
+      <button className="m-4 p-2 bg-yellow-400 rounded">
+        <a href="/auth/google">Sign in with Google</a>
+      </button>
+    </div>
+  );
 }
 
 export default App;
